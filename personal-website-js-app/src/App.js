8c9b9c9de8108cedc70f15477ca5288295c32cde
Bradley Cardona <bradleymathematics@gmail.com>
2022-10-12 15:20:14 -0400

    edit App.js

--- personal-website-js-app/src/App.js
+++ personal-website-js-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MyCard from "./components/general";
+import MyParticle from './components/my-particle'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    visible: true
+  };
+
+  render() {
+    return (
+      <>
+        <MyCard className="App"/>
+        <MyParticle />
+      </>
+    );
+  }
 }
 
 export default App;
